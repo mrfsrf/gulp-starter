aeb5489a98caaba0a01213e7c1108fd5bac92dd8
Luka Juras <mrfsrf@gmail.com>
2020-04-26 07:54:07 +0200

    more modification...

--- gulpfile.js
+++ gulpfile.js
@@ -12,17 +12,29 @@ const gulp = require('gulp'),
   changed = require('gulp-changed'),
   uglify = require('gulp-uglify');
 
+// File location vars  
+const src_dir = "./src",
+      src_js = `${src_dir}/js`,
+      src_sass = `${src_dir}/sass`,
+      assets = "./assets",
+      src_fonts = `${assets}/fonts`,
+      src_images = `${assets}/images`,
+      out_dir = "./public",
+      out_css = `${out_dir}/css`,
+      out_js = `${out_dir}/js`,
+      out_images = `${out_dir}/images`;
+
  
 function style() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', `${src_sass}/**/*.scss`])
     .pipe(sass())
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest(`${out_css}/css`))
     .pipe(browserSync.stream());
 }
  
 function js() {
-  return gulp.src((['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'src/js/*.js']))
-  .pipe(gulp.dest('dist/js'))
+  return gulp.src((['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', `${src_js}/*.js`]))
+  .pipe(gulp.dest(`${out_js}`))
   .pipe(browserSync.stream());
 }
  
@@ -34,8 +46,8 @@ function watch() {
     port: 8080
   });
  
-  gulp.watch('src/scss/*.scss', style).on('change', browserSync.reload);
-  gulp.watch('src/js/*.js', js).on('change', browserSync.reload);
+  gulp.watch(`${src_sass}/**/*.scss`, style).on('change', browserSync.reload);
+  gulp.watch(`${src_js}/*.js`, js).on('change', browserSync.reload);
   gulp.watch('./*.html').on('change', browserSync.reload);
 }
  
