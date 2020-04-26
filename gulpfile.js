ccaa9e237af548f193fc36fc9880ce8f6e7ff802
Luka Juras <mrfsrf@gmail.com>
2020-04-26 12:09:12 +0200

    adding babel and refactoring code...

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const gulp = require('gulp'),
+const { src, dest, series, parallel, watch } = require('gulp'),
+  del = require('del'),
   sass = require('gulp-sass'),
   browserSync = require('browser-sync').create(),
   autoprefixer = require('gulp-autoprefixer'),
@@ -33,22 +34,22 @@ const bootstrap_js = 'node_modules/bootstrap/dist/js/bootstrap.min.js',
 
 function css() {
   // ['node_modules/bootstrap/scss/bootstrap.scss', `${src_sass}/**/*.scss`]
-  return gulp.src([`${src_sass}/**/*.scss`])
+  return src([`${src_sass}/**/*.scss`])
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 1 version'))
     .pipe(sourcemaps.write())
     .pipe(hash()) // rename file using hash
-    .pipe(gulp.dest(`${out_css}`))
+    .pipe(dest(`${out_css}`))
     .pipe(uglify())
     .pipe(rename(function (path) { path.basename += "-min"; }))
-    .pipe(gulp.dest(`${out_css}`))
+    .pipe(dest(`${out_css}`))
     .pipe(browserSync.stream());
 }
 
 // concat styles
 function concatCss() {
-  return gulp.src([`${src_sass}/**/*.scss`])
+  return src([`${src_sass}/**/*.scss`])
     .pipe(concat('main.min.css'))
     .pipe(sourcemaps.init({ loadMaps: true, largeFile: true }))
     .pipe(cleanCSS({debug: true}, (details) => {
@@ -56,24 +57,24 @@ function concatCss() {
       console.log(`${details.name}: ${details.stats.minifiedSize}`);
           }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(`${out_css}`))
+    .pipe(dest(`${out_css}`))
 }
  
 function js() {
   // third party + our js
-  return gulp.src(([`${bootstrap_js}`, `${jquery_js}`, `${src_js}/*.js`]))
+  return src(([`${bootstrap_js}`, `${jquery_js}`, `${src_js}/*.js`]))
   .pipe(concat('main.js'))
   .pipe(sourcemaps.init({ loadMaps: true }))
   .pipe(uglify())
   .pipe(rename(function (path) { path.basename += ".min"; }))
   .pipe(sourcemaps.write('./'))
   // .pipe(size({ title: '  Final JS output:', showFiles: true })) // Show info (size node)
-  .pipe(gulp.dest(`${out_js}`))
+  .pipe(dest(`${out_js}`))
   .pipe(browserSync.stream());
 }
 
 function images() {
-  return gulp.src(`${src_images}/**/*.{jpg,jpeg,png,svg}`)
+  return src(`${src_images}/**/*.{jpg,jpeg,png,svg}`)
     .pipe(changed(`${out_images}`)) // or better {since: gulp.lastRun(images)}
     .pipe(imagemin([  // Basic: .pipe(imagemin())
       pngquant({
@@ -87,24 +88,30 @@ function images() {
         ]
       })
       ]))
-    .pipe(gulp.dest(`${out_images}`))
+    .pipe(dest(`${out_images}`))
     .pipe(hasher());  // hash and cache them
 }
 
-function watch() {
+async function clean() {
+  await del(out_dir);
+}
+
+function watcher() {
   browserSync.init({
     // proxy: "localhost:13500",
+    notify: false,
+    open: false,
     server: {
       baseDir: './'
     },
     port: 8080
   });
  
- // Todo
-  gulp.watch(`${src_sass}/**/*.scss`, gulp.series([css, concatCss])).on('change', browserSync.reload);
-  gulp.watch(`${src_images}/**/*`, images).on('change', browserSync.reload);
-  gulp.watch(`${src_js}/*.js`, js).on('change', browserSync.reload);
-  gulp.watch(['./*.html', `./${out_js}/main.min.js`, `./${out_css}/main.min.css`]).on('change', browserSync.reload);
+  // Todo
+  watch(`${src_sass}/**/*.scss`, series([css, concatCss])).on('change', series(css, concatCss, browserSync.reload));
+  watch(`${src_images}/**/*`, images).on('change', browserSync.reload);
+  watch(`${src_js}/*.js`, js).on('change', browserSync.reload);
+  watch(['./*.html', `./${out_js}/main.min.js`, `./${out_css}/main.min.css`]).on('change', browserSync.reload);
 }
 
 
@@ -113,8 +120,10 @@ exports.css = css;
 exports.concatCss = concatCss;
 exports.js = js;
 exports.images = images;
-exports.watch = watch;
+exports.watcher = watcher;
+exports.clean = clean;
+exports.cleanCss = series(clean, parallel(css, concatCss, images, js), watcher);
 
 // !!!
-const build = gulp.parallel(watch);
+const build = parallel(watcher);
 gulp.task('default', build);
